2e96b9f0773abb8016189d8991a01210b8311505
Jean <jeamsillva3@gmail.com>
2025-06-13 19:09:39 -0300

    :sparkles: adicionada opção de adicionar descrições

--- frontend/src/app/components/TasksForm.tsx
+++ frontend/src/app/components/TasksForm.tsx
@@ -1,12 +1,15 @@
+import { useRef, useState } from "react";
 import { useTasks } from "../hooks/useTask";
 import { ExportTasks } from "./ExportTasks";
 import TaskPrioritySelect from "./TaskPrioritySelect";
 
 export function RenderizarFormTask() {
   const { task, setTask, criarTask } = useTasks();
+  const [showDescription, setShowDescription] = useState(false);
+  const descInputRef = useRef<HTMLTextAreaElement>(null);
 
   return (
-    <div className="flex gap-2 items-center">
+    <div className="flex gap-2">
       <input
         type="text"
         value={task.title}
@@ -14,6 +17,13 @@ export function RenderizarFormTask() {
         placeholder="Tarefa"
         className="border border-zinc-800 px-2 py-1 rounded-md"
         onKeyDown={(e) => {
+          if (e.shiftKey && e.key === "Enter") {
+            e.preventDefault();
+            setShowDescription(true);
+            setTimeout(() => descInputRef.current?.focus(), 100);
+            return;
+          }
+
           if (e.key === "Enter") {
             criarTask();
             (
@@ -21,9 +31,26 @@ export function RenderizarFormTask() {
                 showToast?: (title: string, message: string) => void;
               }
             ).showToast?.("Tarefa criada", "Sua nova tarefa foi adicionada.");
+
+            setTask({
+              title: "",
+              description: "",
+              isCompleted: false,
+              priority: "NONE",
+            });
+            setShowDescription(false);
           }
         }}
       />
+      {showDescription && (
+        <textarea
+          ref={descInputRef}
+          value={task.description || ""}
+          onChange={(e) => setTask({ ...task, description: e.target.value })}
+          placeholder="Descrição da tarefa"
+          className="border border-zinc-800 px-2 py-1 rounded-md w-full"
+        />
+      )}
 
       <TaskPrioritySelect
         priority={task.priority}
@@ -32,7 +59,21 @@ export function RenderizarFormTask() {
 
       {!task.id && (
         <button
-          onClick={criarTask}
+          onClick={() => {
+            criarTask();
+            (
+              window as Window & {
+                showToast?: (title: string, message: string) => void;
+              }
+            ).showToast?.("Tarefa criada", "Sua nova tarefa foi adicionada.");
+            setTask({
+              title: "",
+              description: "",
+              isCompleted: false,
+              priority: "NONE",
+            });
+            setShowDescription(false);
+          }}
           disabled={!task.title.trim()}
           className={`px-2 py-1 rounded-md text-white transition-colors duration-300
             ${
@@ -44,7 +85,6 @@ export function RenderizarFormTask() {
           Criar
         </button>
       )}
-
       <div>
         <ExportTasks />
       </div>
